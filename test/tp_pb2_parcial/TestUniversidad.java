950f6ea669ba3b57bea4c5731047566a3cedbdb8
gonzaviale <gonzaloleonelviale@gmail.com>
2023-09-20 12:19:44 -0300

    commit 2

---
+++ test/tp_pb2_parcial/TestUniversidad.java
@@ -0,0 +1,14 @@
+package tp_pb2_parcial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUniversidad {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
